026c7138dfe5e50731824ac008c470d710e6a2c7
Travis Mendenhall <twmwayne@gmail.com>
2020-10-09 14:01:09 -0600

    added timer, and correct/incorrect functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var counter = 0;
 var correctCount = 0;
 var second_val = 0;
+var high_score = 0.0;
 
 var questionAnswers = {
 
@@ -12,7 +13,7 @@ var questionAnswers = {
     question2: {
         question: "What does html stand for?",
         answers: ["Hypertext Markup Language", "Home Markup Language", "Hypertext Making Language"],
-        correctAnswer: "HyperText Markup Language"
+        correctAnswer: "Hypertext Markup Language"
     },
     question3: {
         question: "What does URL stand for?",
@@ -22,8 +23,8 @@ var questionAnswers = {
 };
 
 var x = setInterval(function() {
-    second_val -= 1;
-    document.getElementById("demo").innerHTML = "Timer" + second_val;
+   second_val -= 1;
+   document.getElementById("demo").innerHTML = "Timer" + second_val;
 }, 1000);
 
 function selectingQuestion(inpQestion) {
@@ -35,6 +36,8 @@ function selectingQuestion(inpQestion) {
 }
 
 function startQuiz() {
+    counter = 0;
+    correctCount = 0;
     document.getElementById("sec1").style.display = 'block';
     document.getElementById("startButton").style.display = 'none';
     document.getElementById("title1").style.display = 'none';
@@ -47,6 +50,16 @@ function startQuiz() {
 var elements = document.getElementsByClassName("quiz-answers");
 
 Array.from(elements).forEach(pass_elem);
+
+
+
+function delay(delaytime) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(2);
+        }, delaytime);
+    })
+}
 
 function pass_elem(element) {
     element.addEventListener('click', function () {
@@ -63,17 +76,46 @@ function pass_elem(element) {
 
         if (correctAnswer == this.textContent) {
             correctCount++;
+            document.getElementById("result").innerHTML = "Correct";
+            document.getElementById("result").style.display = 'block';
         }
+        else {
+            document.getElementById("result").innerHTML = "Wrong";
+            document.getElementById("result").style.display = 'block';
+        }
+
+        setTimeout(function() {  
+            document.getElementById("result").style.display = 'none';
+        }, 2000);
+        
 
         if (counter == 1) {
             selectingQuestion(questionAnswers.question2);
         }
-        else {
+        else if (counter == 2){
             selectingQuestion(questionAnswers.question3);
+        }
+        else {
+            var score = correctCount / 3.0;
+            var sval = Number(score).toLocaleString(undefined, {style: 'percent', minimumFractionDigits:0});
+            //document.getElementById("result").innerHTML = "Score: " + sval + "; with a time of: " + second_val;
+            //document.getElementById("result").style.display = "block";
+            document.getElementById("sec2").style.display = 'block';
+            document.getElementById("demo").style.display = 'none';
+
+            if (score > high_score )
+                high_score = score;
         }
     })
     
 }
+
+function startOver() {
+    document.getElementById("result").style.display = "none";
+    document.getElementById("sec2").style.display = "none";
+    startQuiz();
+}
+
 
 
 
